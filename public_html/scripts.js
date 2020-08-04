74cfc1318ccecea6fc430e475de6410287276b67
Jordan Hicks <jhicks@jhicksdesign.com>
2020-08-03 19:33:11 -0600

    Updated padding and banner formatting.

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,30 +1,30 @@
 const gallerySwitch = (ev) => {
-    const buttonOne = document.getElementById("button-one");
-    const buttonTwo = document.getElementById("button-two");
-    const buttonThree = document.getElementById("button-three");
-    const firstRow = document.getElementById("first-row");
-    const secondRow = document.getElementById("second-row");
-    const thirdRow = document.getElementById("third-row");
-    if (ev === buttonOne) {
-        buttonOne.disabled = true;
-        buttonTwo.disabled = false;
-        buttonThree.disabled = false;
-        firstRow.classList.remove("d-none")
-        secondRow.classList.add("invisible")
-        thirdRow.classList.add("d-none")
-    } else if (ev === buttonTwo) {
-        buttonOne.disabled = false;
-        buttonTwo.disabled = true;
-        buttonThree.disabled = false;
-        firstRow.classList.add("d-none")
-        secondRow.classList.remove("d-none")
-        thirdRow.classList.add("d-none")
-    } else {
-        buttonOne.disabled = false;
-        buttonTwo.disabled = false;
-        buttonThree.disabled = true;
-        firstRow.classList.add("d-none")
-        secondRow.classList.add("d-none")
-        thirdRow.classList.remove("d-none")
-    }
+  const buttonOne = document.getElementById("button-one");
+  const buttonTwo = document.getElementById("button-two");
+  const buttonThree = document.getElementById("button-three");
+  const firstRow = document.getElementById("first-row");
+  const secondRow = document.getElementById("second-row");
+  const thirdRow = document.getElementById("third-row");
+  if (ev === buttonOne) {
+    buttonOne.disabled = true;
+    buttonTwo.disabled = false;
+    buttonThree.disabled = false;
+    firstRow.classList.remove("d-none")
+    secondRow.classList.add("invisible")
+    thirdRow.classList.add("d-none")
+  } else if (ev === buttonTwo) {
+    buttonOne.disabled = false;
+    buttonTwo.disabled = true;
+    buttonThree.disabled = false;
+    firstRow.classList.add("d-none")
+    secondRow.classList.remove("d-none")
+    thirdRow.classList.add("d-none")
+  } else {
+    buttonOne.disabled = false;
+    buttonTwo.disabled = false;
+    buttonThree.disabled = true;
+    firstRow.classList.add("d-none")
+    secondRow.classList.add("d-none")
+    thirdRow.classList.remove("d-none")
+  }
 }
